2d7f26c894d2e4c6e7ca1be2823ab7455633c523
yeonjujeong0511 <yeonjujeong0511@gmail.com>
2023-03-13 14:41:58 +0900

    변경사항

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://run.mocky.io/v3/bf256690-9c16-44ad-9ee0-51cb26d76405",
+  baseURL: "http://localhost:8080",
 });
 
 function registerUser(userData) {
-  return instance.post("", userData);
+  return instance.post("/addMember", userData);
 }
 
 function loginUser(userData) {
-  return instance.post("", userData);
+  return instance.post("/login", userData);
 }
 
 export { registerUser, loginUser };
