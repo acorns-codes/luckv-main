e91b1e86244877ff80744801e633d6898636196a
bitnal-h <thgml4890@naver.com>
2023-03-24 11:40:49 +0900

    스케쥴러

--- luckv/src/main/java/com/luckv/demo/LuckVApplication.java
+++ luckv/src/main/java/com/luckv/demo/LuckVApplication.java
@@ -2,7 +2,9 @@ package com.luckv.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class LuckVApplication {
 
