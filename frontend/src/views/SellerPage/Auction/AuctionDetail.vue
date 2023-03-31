10de6fae1569bb2f225bc3d96cade914bdc3263f
bitnal-h <thgml4890@naver.com>
2023-03-31 18:50:16 +0900

    커밋할게있나?

--- frontend/src/views/SellerPage/Auction/AuctionDetail.vue
+++ frontend/src/views/SellerPage/Auction/AuctionDetail.vue
@@ -39,90 +39,65 @@
                     ></v-text-field>
                   </td>
                 </tr>
-                <template v-if="this.auctionData.kind === '경매'">
-                  <tr>
-                    <th>경매 시작가</th>
-                    <td>
-                      <v-text-field
-                        variant="plain"
-                        readonly
-                        v-model="this.auctionData.payStart"
-                        suffix="원"
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>경매 시작 날짜</th>
-                    <td>
-                      <v-text-field
-                        v-model="this.startDay[0]"
-                        variant="plain"
-                        readonly
-                        type="date"
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>경매 시작 시간</th>
-                    <td>
-                      <v-text-field
-                        variant="plain"
-                        v-model="this.startDay[1]"
-                        type="time"
-                        readonly
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>경매 마감 날짜</th>
-                    <td>
-                      <v-text-field
-                        v-model="this.lastDay[0]"
-                        variant="plain"
-                        readonly
-                        type="date"
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>경매 마감 시간</th>
-                    <td>
-                      <v-text-field
-                        variant="plain"
-                        v-model="this.lastDay[1]"
-                        type="time"
-                        readonly
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                </template>
-                <template v-else-if="this.auctionData.kind === '구독'">
-                  <tr>
-                    <th>구독 마감 날짜</th>
-                    <td>
-                      <v-text-field
-                        v-model="this.lastDay[0]"
-                        variant="plain"
-                        readonly
-                        type="date"
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>구독 마감 시간</th>
-                    <td>
-                      <v-text-field
-                        variant="plain"
-                        v-model="this.lastDay[1]"
-                        type="time"
-                        readonly
-                      ></v-text-field>
-                    </td>
-                  </tr>
-                </template>
+
+                <tr>
+                  <th>경매 시작가</th>
+                  <td>
+                    <v-text-field
+                      variant="plain"
+                      readonly
+                      v-model="this.auctionData.payStart"
+                      suffix="원"
+                    ></v-text-field>
+                  </td>
+                </tr>
+                <tr>
+                  <th>경매 시작 날짜</th>
+                  <td>
+                    <v-text-field
+                      v-model="this.startDay[0]"
+                      variant="plain"
+                      readonly
+                      type="date"
+                    ></v-text-field>
+                  </td>
+                </tr>
+                <tr>
+                  <th>경매 시작 시간</th>
+                  <td>
+                    <v-text-field
+                      variant="plain"
+                      v-model="this.startDay[1]"
+                      type="time"
+                      readonly
+                    ></v-text-field>
+                  </td>
+                </tr>
+                <tr>
+                  <th>경매 마감 날짜</th>
+                  <td>
+                    <v-text-field
+                      v-model="this.lastDay[0]"
+                      variant="plain"
+                      readonly
+                      type="date"
+                    ></v-text-field>
+                  </td>
+                </tr>
+                <tr>
+                  <th>경매 마감 시간</th>
+                  <td>
+                    <v-text-field
+                      variant="plain"
+                      v-model="this.lastDay[1]"
+                      type="time"
+                      readonly
+                    ></v-text-field>
+                  </td>
+                </tr>
               </thead>
             </v-table>
-            <div v-show="this.auctionData.kind !== '무료'">
+            <div>
               <v-table class="table-box">
                 <thead>
                   <tr>
@@ -135,15 +110,24 @@
                       ></v-text-field>
                     </td>
                   </tr>
-
-                  <tr v-show="this.auctionData.kind === '경매'">
+                  <tr>
+                    <th>낙찰자</th>
+                    <td>
+                      <v-text-field
+                        variant="plain"
+                        readonly
+                        v-model="this.auctionData.buyerNm"
+                      ></v-text-field>
+                    </td>
+                  </tr>
+                  <tr>
                     <th>최고가</th>
                     <td>
                       <v-text-field
                         suffix="원"
                         readonly
                         variant="plain"
-                        v-model="this.auctionData.payMax"
+                        v-model="pay"
                       ></v-text-field>
                     </td>
                   </tr>
@@ -151,62 +135,7 @@
               </v-table>
             </div>
           </div>
-          <v-btn
-            v-if="this.auctionData.status !== '판매종료'"
-            @click="editAuction"
-            >수정</v-btn
-          >
-
-          <v-dialog v-else v-model="dialog" persistent width="500">
-            <template v-slot:activator="{ props }">
-              <v-btn variant="outlined" color="#FF9414" v-bind="props">
-                구독으로 변경
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">구독 동영상으로 변경</span>
-              </v-card-title>
-              <v-card-text>
-                <v-form v-model="valid" @submit.prevent="sendMessage">
-                  <p>동영상 제공 기간</p>
-                  <v-text-field
-                    label="날짜"
-                    v-model="Day"
-                    type="date"
-                    :rules="lastDaytRules"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    label="시간"
-                    v-model="Time"
-                    type="time"
-                    :rules="lastTimetRules"
-                    required
-                  ></v-text-field>
-
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      color="blue-darken-1"
-                      variant="text"
-                      @click="dialog = false"
-                    >
-                      닫기
-                    </v-btn>
-                    <v-btn
-                      type="submit"
-                      color="blue-darken-1"
-                      variant="text"
-                      @click="dialog = false"
-                    >
-                      변경
-                    </v-btn>
-                  </v-card-actions>
-                </v-form>
-              </v-card-text>
-            </v-card>
-          </v-dialog>
+          <v-btn @click="editAuction">수정</v-btn>
         </div>
       </div>
       <!-- <v-btn @click="sendMessage">소켓으로 보내기</v-btn>
@@ -219,7 +148,8 @@
 
 <script>
 import MypageNav from "@/components/MypageNav.vue";
-
+import Stomp from "webstomp-client";
+import SockJS from "sockjs-client";
 export default {
   components: { MypageNav },
   data() {
@@ -231,14 +161,35 @@ export default {
       lastDay: "",
       buyer: "",
       recvList: [],
-
-      dialog: false,
-      valid: false,
-      Day: "",
-      Time: "",
       wepsocket: "",
-
+      categorys: [
+        {
+          title: "동물",
+          value: "animal",
+        },
+        {
+          title: "인물",
+          value: "character",
+        },
+        {
+          title: "건물",
+          value: "building",
+        },
+        {
+          title: "식물",
+          value: "plant",
+        },
+        {
+          title: "기타",
+          value: "etc",
+        },
+      ],
     };
+  },
+
+  created() {
+    // 소켓 연결 시도
+    this.connect();
   },
   mounted() {
     // 상세 내역 불러오기
@@ -248,7 +199,7 @@ export default {
     // 소켓 연결
     connect() {
       console.log("소켓연결");
-      const serverURL = `${process.env.VUE_APP_API_URL}`;
+      const serverURL = `http://localhost:80`;
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
       console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`);
@@ -319,7 +270,6 @@ export default {
       }
       console.log(this.recvList, "받아온데이터어어엉어어어3");
     },
-
     // 각 경매의 상세페이지 받아오기
     async getAuction() {
       console.log("경매조회");
@@ -330,67 +280,17 @@ export default {
         });
         console.log(res);
         this.auctionData = res.data.data;
-        this.auctionData.vcate =
-          this.auctionData.vcate === "animal"
-            ? "동물"
-            : this.auctionData.vcate === "chracter"
-            ? "인물"
-            : this.auctionData.vcate === "building"
-            ? "건물"
-            : this.auctionData.vcate === "plant"
-            ? "식물"
-            : "기타";
         this.startDay = this.auctionData.startDay.split(" ");
         this.lastDay = this.auctionData.lastDay.split(" ");
       } catch (error) {
         console.log(error);
       }
     },
-    // 수정 페이지로 이동
     editAuction() {
       this.$router.push({
         name: "editauction",
         params: { ano: this.$route.params.ano },
       });
-    },
-    // 구독으로 넘길지 확인
-    confirmToSub() {
-      if (
-        !confirm(
-          "종료된 경매의 입찰을 취소하고 동영상을 구독으로 넘기시겠습니까?"
-        )
-      ) {
-        console.log("아니요");
-      } else {
-        console.log("구독으로 넘기기");
-        this.moveToSub();
-      }
-    },
-    // 구독으로 넘기는 함수
-    async moveToSub() {
-      console.log("구독으로 넘겨오오오오오");
-      const editData = {
-        ano: this.auctionData.ano,
-      };
-      try {
-        if (!this.valid) {
-          console.log(this.valid);
-          alert("제공 기간을 확인해주세요!");
-          return;
-        } else {
-          const res = this.$axios({
-            headers: {
-              "Content-type": "application/json",
-            },
-            method: "POST",
-            url: `${process.env.VUE_APP_API_URL}/auctionChange`,
-            data: editData,
-          });
-          console.log(res);
-        }
-      } catch (error) {
-        console.log(error);
-      }
     },
   },
 };
@@ -414,15 +314,15 @@ export default {
   /* width: 100%; */
   flex-direction: column;
   display: flex;
+  /* align-items: center; */
   padding-bottom: 10px;
 }
 .container {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  /* align-items: center; */
   width: 100%;
   padding-top: 110px;
-  margin-bottom: 50px;
 }
 .table-box {
   width: 700px;
@@ -451,7 +351,7 @@ export default {
   margin-bottom: 30px;
 }
 
-/* button {
+button {
   margin: 20px;
-} */
+}
 </style>
